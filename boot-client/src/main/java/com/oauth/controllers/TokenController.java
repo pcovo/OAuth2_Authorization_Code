69e0008ee0b94b1a0b27ca166a29ea80163987e0
paula.covo <paula.covo@GPSPNB005361.gpnet.com.br>
2019-01-16 15:15:42 -0200

    Isolando o Serviço de Geração de Token

--- boot-client/src/main/java/com/oauth/controllers/TokenController.java
+++ boot-client/src/main/java/com/oauth/controllers/TokenController.java
@@ -1,21 +1,23 @@
 package com.oauth.controllers;
 
 import java.io.IOException;
-import java.security.Principal;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -24,18 +26,38 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Controller
 public class TokenController {
 
-	@RequestMapping(value="/usuario", method = RequestMethod.GET)
-	public Principal usuario(Principal principal) {
-	    return principal;
-	}
-	
-	@RequestMapping(value = "/getToken", method = RequestMethod.GET)
-	public ModelAndView getEmployeeInfo() {
-		return new ModelAndView("solicitarToken");
+	/**
+	 * Documentação Brasil Cidadão (brasilcidadao-software-development-kit-v1.8.pdf – página 13)
+		A requisição é feita através de um GET para o endereço 
+		https://(url do provider brasil cidadão)/scp/authorize  
+		passando as seguintes informações: 
+		1. Parâmetros query: 
+		◦ response_type: code; 
+		◦ client_id: <CLIENT_ID> fornecido pelo Brasil Cidadão para a aplicação cadastrada; 
+		◦ scope: um ou mais escopos inseridos para a aplicação cadastrada. Se for mais de um, esta informação deve vir separada pelo caracter "+". 
+		◦ redirect_uri: <URI de retorno cadastrada para a aplicação cliente>. 
+		◦ nonce: <sequência alfa numérica aleatória>. 
+		◦ state: <sequência alfa numérica aleatória>. Item não obrigatório.
+		Exemplo de requisição:
+		https://(url do provider brasil cidadão)/scp/authorize?response_type=code&client_id=ec4318d6f797-4d65-b4f7-39a33bf4d544&scope=openid+brasil_cidadao& redirect_uri=http://appcliente.com.br/phpcliente/loginecidadao.Php&nonce=3ed8657fd74c&state=3 58578ce6728b
+	 */
+	@RequestMapping(value = "/getTokenService", method = RequestMethod.GET)
+	public ResponseEntity<Object> getTokenService() throws Exception {
+			URI uri = null;
+			try {
+				uri = new URI("http://localhost:8080/oauth/authorize?response_type=code&client_id=client-id&redirect_uri=http://localhost:8090/setToken&scope=read&state=123456");
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+				throw e;
+			}
+			HttpHeaders httpHeaders = new HttpHeaders();
+			httpHeaders.setLocation(uri);
+			return (new ResponseEntity<>(httpHeaders, HttpStatus.TEMPORARY_REDIRECT));
 	}
 	
 	@RequestMapping(value = "/setToken", method = RequestMethod.GET)
-	public ModelAndView setToken(@RequestParam("code") String code, @RequestParam("state") String state) throws JsonProcessingException, IOException {
+	@ResponseBody
+	public String setToken(@RequestParam("code") String code, @RequestParam("state") String state) throws JsonProcessingException, IOException {
 		ResponseEntity<String> response = null;
 
 		RestTemplate restTemplate = new RestTemplate();
@@ -60,9 +82,7 @@ public class TokenController {
 		JsonNode node = mapper.readTree(response.getBody());
 		String token = node.path("access_token").asText();
 		
-		ModelAndView model = new ModelAndView("receberToken");
-		model.addObject("token",token);
-		return model;
+		return token;
 	}
 }
 
--- boot-client/src/main/webapp/WEB-INF/jsp/receberToken.jsp
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title>Token Retornado</title>
-</head>
-<body>
-	<h3 style="color: blue;">Token</h3>	
-	${token}
-</body>
-</html>
